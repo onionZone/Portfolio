8df358931713d58e9de953f5f1929ce3646c325b
onionZone <jakceb@gmail.com>
2024-01-29 09:58:35 +0100

    fix: images

--- src/components/Skills/config/config.ts
+++ src/components/Skills/config/config.ts
@@ -5,21 +5,29 @@ interface Skill {
 }
 
 export const config: Skill[] = [
-  { name: "HTML", level: "80", image: "../../../../public/img/html.svg" },
-  { name: "CSS", level: "90", image: "../../../../public/img/css.svg" },
-  { name: "JavaScript", level: "70", image: "../../../../public/img/js.svg" },
-  { name: "TypeScript", level: "70", image: "../../../../public/img/ts.svg" },
-  { name: "React", level: "65", image: "../../../../public/img/react.svg" },
-  { name: "Redux", level: "65", image: "../../../../public/img/redux.svg" },
+  { name: "HTML", level: "80", image: "/../../../../public/img/html.svg" },
+  { name: "CSS", level: "90", image: "/../../../../public/img/css.svg" },
+  { name: "JavaScript", level: "70", image: "/../../../../public/img/js.svg" },
+  { name: "TypeScript", level: "70", image: "/../../../../public/img/ts.svg" },
+  { name: "React", level: "65", image: "/../../../../public/img/react.svg" },
+  { name: "Redux", level: "65", image: "/../../../../public/img/redux.svg" },
   {
     name: "Bootstrap",
     level: "65",
-    image: "../../../../public/img/bootstrap.svg",
+    image: "/../../../../public/img/bootstrap.svg",
   },
-  { name: "SASS", level: "65", image: "../../../../public/img/sass.svg" },
-  { name: "Material UI", level: "65", image: "../../../../public/img/mui.svg" },
-  { name: "Cypress", level: "65", image: "../../../../public/img/cypress.svg" },
-  { name: "Vitest", level: "65", image: "../../../../public/img/vitest.svg" },
-  { name: "Vite", level: "65", image: "../../../../public/img/vite.svg" },
-  { name: "Docker", level: "65", image: "../../../../public/img/docker.svg" },
+  { name: "SASS", level: "65", image: "/../../../../public/img/sass.svg" },
+  {
+    name: "Material UI",
+    level: "65",
+    image: "/../../../../public/img/mui.svg",
+  },
+  {
+    name: "Cypress",
+    level: "65",
+    image: "/../../../../public/img/cypress.svg",
+  },
+  { name: "Vitest", level: "65", image: "/../../../../public/img/vitest.svg" },
+  { name: "Vite", level: "65", image: "/../../../../public/img/vite.svg" },
+  { name: "Docker", level: "65", image: "/../../../../public/img/docker.svg" },
 ];
