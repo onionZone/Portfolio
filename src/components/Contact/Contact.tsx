a1c2d96a910c7bf4b43b9e7940aeaacee0716ae0
onionZone <jakceb@gmail.com>
2024-01-31 18:40:32 +0100

    chore: contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,8 +1,27 @@
+import "./Contact.css";
+
 export const Contact = () => {
   return (
-    <div id="container" className="container">
+    <div id="contact" className="container">
       <h1>Contact Me</h1>
-      <div>In progress..</div>
+      <form
+        target="_blank"
+        action="https://formsubmit.co/68438053b8295a46a8a335464bc5fde0"
+        method="POST"
+      >
+        <label>Name:</label>
+        <input type="text" name="name" placeholder="Write your name.." />
+        <label>E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Write your email.."
+          required
+        />
+        <label>Your message:</label>
+        <textarea placeholder="Write your message.." rows={10} required />
+        <button type="submit">Send Message</button>
+      </form>
     </div>
   );
 };
